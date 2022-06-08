2061fee1566a40db19b1f4d0074b099b97548133
Guilherme Santos <agustinho_santos@hotmail.com>
2022-06-08 18:32:13 -0300

    page wallet ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'wallet', to: 'pages#wallet'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :companies do
     resources :tokens, only: %i[new create]
