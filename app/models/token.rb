73bd2bbac81b929b4d382edadaa3116bd3bb3698
Guilherme Santos <agustinho_santos@hotmail.com>
2022-06-08 15:51:49 -0300

    page buy ok e lógica finalizada

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,4 +1,14 @@
 class Token < ApplicationRecord
   belongs_to :user
   belongs_to :company
+
+  validate :check_max_tokens
+
+  private
+
+  def check_max_tokens
+    if company.sold_tokens + purchased_tokens > company.max_tokens
+      errors.add(:purchased_tokens, "Insufficient amount of tokens!")
+    end
+  end
 end
