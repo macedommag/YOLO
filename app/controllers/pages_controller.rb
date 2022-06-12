6abb9acbae7f1a824b6a5955165b26f356cc1af9
Guilherme Santos <agustinho_santos@hotmail.com>
2022-06-12 00:12:39 -0300

    page wallet total investido, total tokens, total empresas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,8 @@ class PagesController < ApplicationController
 
   def wallet
     @companies = current_user.tokens.map { |token| token.company }.uniq
+    @total_tokens = current_user.tokens.map { |token| token.purchased_tokens }.sum
+    @total_company= current_user.tokens.map { |token| token.company }.uniq.count
+    @total_invest = current_user.tokens.map { |token| token.company.sold_tokens * token.company.price_of_token }.uniq.sum
   end
 end
