8c81a9a2b867f6db56b5a965153d0be509be8d7a
rodrigo araujo santos <araujo.1983@hotmail.com>
2022-06-14 12:21:46 -0300

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_06_13_183921) do
-=======
-ActiveRecord::Schema.define(version: 2022_06_13_140022) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
