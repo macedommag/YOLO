bef3dcbf92db7a8662da6c4c03e4abcf6d0de60f
Guilherme Santos <agustinho_santos@hotmail.com>
2022-06-14 15:46:03 -0300

    page-index-cards && validaçoes back-end

--- app/controllers/user_tokens_controller.rb
+++ app/controllers/user_tokens_controller.rb
@@ -10,10 +10,12 @@ class UserTokensController < ApplicationController
     new_token.company = @token.company
     new_token.peer_to_peer = true
     new_token.quantity_purchased = params[:token][:purchased_tokens].to_i
-    if new_token.save
-      @token.purchased_tokens -= new_token.quantity_purchased
-      @token.peer_to_peer = true
-      @token.quantity_purchased = 0 #zero porque está vendendo e não comprando
+    #ATUALIZAÇÃO DO TOKEN ORIGINAL
+    @token.purchased_tokens -= new_token.quantity_purchased
+    @token.peer_to_peer = true
+    @token.quantity_purchased = 0 #zero porque está vendendo e não comprando
+    if new_token.valid? && @token.valid?
+      new_token.save
       @token.save!
       redirect_to token_path(new_token)
     else
